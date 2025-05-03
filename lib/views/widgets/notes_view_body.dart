8472acc9daca4eb31b5133e75f038dc248dd612a
Salmamoh22 <salma.muhamed2910@gmail.com>
2025-05-03 19:30:19 +0200

    show model bottom sheet

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_app_bar.dart';
-import 'package:notes_app/views/widgets/custom_note_item.dart';
-import 'package:notes_app/views/widgets/custom_notes_list_view.dart';
+import 'package:notes_app/views/widgets/notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -15,7 +14,7 @@ class NotesViewBody extends StatelessWidget {
           SizedBox(height: 50),
           CustomAppBar(),
           SizedBox(height: 10),
-          Expanded(child: CustomNotesListView()),
+          Expanded(child: NotesListView()),
         ],
       ),
     );
