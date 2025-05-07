91ac5c0117876917e3766a20049feb985d1d0f41
Salmamoh22 <salma.muhamed2910@gmail.com>
2025-05-08 01:20:40 +0200

    Edit Note Done

---
+++ lib/views/widgets/edit_note_view_body.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
+import 'package:notes_app/models/note_model.dart';
+import 'package:notes_app/views/widgets/custom_app_bar.dart';
+import 'package:notes_app/views/widgets/custom_text_form_field.dart';
+
+class EditNoteViewBody extends StatefulWidget {
+  const EditNoteViewBody({super.key, required this.note});
+  final NoteModel note;
+
+  @override
+  State<EditNoteViewBody> createState() => _EditNoteViewBodyState();
+}
+
+class _EditNoteViewBodyState extends State<EditNoteViewBody> {
+  String? title, content;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: [
+          const SizedBox(height: 50),
+          CustomAppBar(
+            onPressed: () {
+              widget.note.title = title ?? widget.note.title;
+              widget.note.subTitle = content ?? widget.note.subTitle;
+              widget.note.save();
+              BlocProvider.of<NotesCubit>(context).fetchAllNotes();
+              Navigator.pop(context);
+            },
+            title: 'Edit Note',
+            icon: FontAwesomeIcons.check,
+          ),
+          const SizedBox(height: 50),
+          CustomTextFormField(
+            hintText: widget.note.title,
+            onChanged: (value) {
+              title = value;
+            },
+          ),
+          const SizedBox(height: 16),
+          CustomTextFormField(
+            hintText: widget.note.subTitle,
+            maxLines: 5,
+            onChanged: (value) {
+              content = value;
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
