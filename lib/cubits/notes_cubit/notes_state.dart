928355af548097eaaff730f4c516550dd0710383
Salmamoh22 <salma.muhamed2910@gmail.com>
2025-05-07 22:59:13 +0200

    Display Notes Data

--- lib/cubits/notes_cubit/notes_state.dart
+++ lib/cubits/notes_cubit/notes_state.dart
@@ -5,18 +5,7 @@ sealed class NotesState {}
 
 final class NotesInitial extends NotesState {}
 
-final class NotesLoading extends NotesState {
-  
-}
 
-final class NotesFailure extends NotesState {
-  final String errorMessage;
 
-  NotesFailure(this.errorMessage);
-}
 
-final class NotesSuccess extends NotesState {
-  final List<NoteModel> note;
 
-  NotesSuccess(this.note);
-}
--- lib/views/widgets/note_item.dart
+++ lib/views/widgets/note_item.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/edit_notes_view.dart';
 
-class CustomNoteItem extends StatelessWidget {
-  const CustomNoteItem({super.key});
-
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key, required this.note});
+  final NoteModel note;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -21,21 +22,21 @@ class CustomNoteItem extends StatelessWidget {
       child: Container(
         padding: EdgeInsets.only(top: 24, bottom: 24, left: 16),
         decoration: BoxDecoration(
-          color: const Color(0xffFFCC80),
+          color: Color(note.color),
           borderRadius: BorderRadius.circular(16),
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             ListTile(
-              title: const Text(
-                'Flutter Tips',
+              title:  Text(
+                note.title,
                 style: TextStyle(color: Colors.black, fontSize: 26),
               ),
               subtitle: Padding(
                 padding: const EdgeInsets.only(top: 16, bottom: 16),
                 child: Text(
-                  'Build your career',
+                  note.subTitle,
                   style: TextStyle(
                     color: Colors.black.withOpacity(0.5),
                     fontSize: 18,
@@ -54,7 +55,7 @@ class CustomNoteItem extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.only(right: 24),
               child: Text(
-                'May 3, 2025',
+                note.date,
                 style: TextStyle(
                   color: Colors.black.withOpacity(0.5),
                   fontSize: 16,
